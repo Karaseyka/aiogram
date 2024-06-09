49f37e341645cc3fae9ab1e1b7fcda01437745a6
Karaseyka <vladchuprina12@mail.ru>
2024-06-09 13:58:30 +0300

    final

--- keyboards.py
+++ keyboards.py
@@ -1,11 +1,10 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
 
-main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Меню')],
-                                     [KeyboardButton(text='Какое-то меню')],
-                                     [KeyboardButton(text='Ещё один пункт меню'),
-                                      KeyboardButton(text='И ещё')]],
-                           resize_keyboard=True,
-                           input_field_placeholder='Выберите пункт меню...')
+main = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Меню', callback_data="to_main")],
+                                      [InlineKeyboardButton(text='Какое-то меню', callback_data="to_main")],
+                                      [InlineKeyboardButton(text='Ещё один пункт меню', callback_data="to_main")],
+                                       [InlineKeyboardButton(text='И ещё', callback_data="to_main")]])
+
 
 contactKeyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Поделиться номером", request_contact=True)]])
